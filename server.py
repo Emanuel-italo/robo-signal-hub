a5e337903cb8046bf74da7ef4efeda5e46118f8c
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-11-30 11:09:17 -0300

    arquivo correto, front e back no mesmo diretorio!!

--- server.py
+++ server.py
@@ -670,4 +670,4 @@ def stop_bot():
     return {"status": "not_running"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=False)
